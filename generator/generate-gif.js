1851f213b854535c39cb0c81b6c3536768cf160d
Victor Powell <vicapow@gmail.com>
2014-03-02 01:59:46 -0800

    finished up proof of concept badge visualization workflow

--- generator/generate-gif.js
+++ generator/generate-gif.js
@@ -7,29 +7,37 @@
 // page.clipRect = { top: 0, left: 0, width: 900, height: 800};
 // page.viewportSize = { width: 900, height: 800};
 
-// var url = 'http://dl.dropbox.com/u/621993/voronoi/voronoi.html';
+var url = 'http://localhost:8080/index.html';
 
-var frames = 10;
-console.log('Loading a web page');
-var page = require('webpage').create();
+var frames = 10
+var frame = 0
+console.log('Loading a web page')
+var page = require('webpage').create()
 
-page.clipRect = { top: 0, left: 0, width: 900, height: 800};
-page.viewportSize = { width: 900, height: 800};
+var w = 1050, h = 1500
+page.clipRect = { top: 0, left: 0, width: w, height: h}
+page.viewportSize = { width: w, height: h}
 
-var url = 'http://www.google.de/';
-page.open(url, function(){
-    console.log("Hello");
-    setInterval(function(){
-        
+// var url = 'http://www.google.com/'
 
-        document.body.style.background = "red";
-        page.render("gif/test-"+frames+".jpg", { format: "jpg" });
-
-        if( frames === 0 ){
-            phantom.exit();
-        }
-
-        frames--;
-
-     }, 100);
- });
+console.log('about to open page')
+page.open(url, function(status){
+  console.log('open page')
+  console.log('status', status)
+  console.log('evaluating')
+  function ready(){
+    console.log('ready!')
+    for(frame = 0; frame < frames; frame++){
+      page.evaluate(function(){ if(window.step) window.step() })
+      page.render("gif/test-"+ frame +".png", { format: "png" })
+    }
+    phantom.exit()
+  }
+  function checkReady(){
+    console.log('is_ready', is_ready)
+    var is_ready = page.evaluate(function() { return window.is_ready })
+    if(is_ready) ready()
+    else setTimeout(checkReady, 10)
+  }
+  checkReady()
+ })
