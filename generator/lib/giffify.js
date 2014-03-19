ca11d32f7e961981ffec16a8b61f39d6113d2f14
Victor Powell <vicapow@gmail.com>
2014-03-18 17:38:19 -0700

    finished giffify. turns badge frames into gifs (and thumbnail gifs for testing)

--- generator/lib/giffify.js
+++ generator/lib/giffify.js
@@ -7,6 +7,18 @@ var spawn = require('child_process').spawn
 function convert(dir, done){
   var convert = spawn('convert', [
     '-loop', '0', dir + '/frame-*.jpg', dir + '/badge.gif' ])
+  convert.stdout.pipe(process.stdout)
+  convert.stderr.pipe(process.stderr)
+  convert.on('close', done)
+  convert.on('error', done)
+}
+
+function create_thumb(dir, done){
+  var args = [ dir + '/badge.gif', '-resize', '100', dir + '/badge-small.gif']
+  console.log('using command', args.join(' '))
+  var convert = spawn('convert', args)
+  convert.stdout.pipe(process.stdout)
+  convert.stderr.pipe(process.stderr)
   convert.on('close', done)
   convert.on('error', done)
 }
@@ -15,8 +27,11 @@ var dirs = JSON.parse(fs.readFileSync('./data/bin/frame-dirs.json'))
 
 ;(function process(err){
   if(err) throw err
-  if(!frameDirs.length) return
-  var next = frameDirs.pop()
+  if(!dirs.length) return
+  var next = dirs.pop()
   console.log('creating gif for ' + next)
-  conver(next, process)
+  convert(next, function(err){
+    if(err) throw err
+    create_thumb(next, process)
+  })
 })()
